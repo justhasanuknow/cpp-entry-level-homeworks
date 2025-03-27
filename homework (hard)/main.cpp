36c051683496f05ab541b6b612631a9959e3e947
M.Hasan Yumuşak <mhymsk1998@hotmail.com>
2025-03-28 01:38:30 +0300

    updated main code

--- homework (hard)/main.cpp
+++ homework (hard)/main.cpp
@@ -2,187 +2,102 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 namespace fs = std::filesystem;
 
 struct Account {
   std::string account_name;
   double account_balance;
-  bool is_account_active;
 };
 
-void create_account();
+void create_account(std::vector<Account> *);
 void transfer_money();
-void view_account_transactions();
-
+void view_account_transactions(std::vector<Account> *);
 void exit_program();
 
 int main() {
-  fs::create_directories("accounts");
+  std::vector<Account> *account_db = new std::vector<Account>;
+  int option{0};
 
-  while (true) {
-    int option{0};
+  std::cout << "\nWelcome to Simple Bank Management System\n\n"
+            << "1. Create Account" << "\n"
+            << "2. Transfer Money" << "\n"
+            << "3. View Account Transactions" << "\n"
+            << "4. Exit" << "\n";
 
-    std::cout << "\n"
-              << "Welcome to Simple Bank Management System" << "\n"
-              << "\n";
-    std::cout << "1. Create Account" << "\n";
-    std::cout << "2. Transfer Money" << "\n";
-    std::cout << "3. View Account Transactions" << "\n";
-    std::cout << "4. Exit" << "\n";
+  std::cout << "Choose an option: ";
+  std::cin >> option;
+  std::cout << "\n";
 
-    std::cout << "Choose an option: ";
-    std::cin >> option;
-    std::cout << "\n";
+  if (std::cin.fail()) {
+    std::cout << "Invalid input! Not a number." << std::endl;
 
-    if (std::cin.fail()) {
-      std::cout << "Invalid input! Not a number." << std::endl;
-      break;
-    }
+    return 1;
+  }
 
-    if (option == 4) {
-      exit_program();
-      break;
-    }
+  if (option == 4) {
+    exit_program();
 
-    switch (option) {
-    // Create account.
-    case 1:
-      create_account();
-      break;
+    return 0;
+  }
 
-    // Transfer money.
-    case 2:
-      transfer_money();
-      break;
+  switch (option) {
+  // Create account.
+  case 1:
+    create_account(account_db);
+    break;
 
-    // View account transactions.
-    case 3:
-      view_account_transactions();
-      break;
+  // Transfer money.
+  case 2:
+    transfer_money();
+    break;
+
+  // View account transactions.
+  case 3:
+    view_account_transactions(account_db);
+    break;
 
     // Invalid option.
-    default:
-      std::cout << "Please enter a valid option!" << "\n";
-      break;
-    }
+  default:
+    std::cout << "Please enter a valid option!" << "\n";
+    break;
   }
 
   return 0;
 }
 
-void create_account() {
+void create_account(std::vector<Account> *account_db) {
+  // std::cout << "Create account!" << std::endl;
   std::string account_name{""};
-  double initial_balance{0.0};
+  double initial_balance{0};
 
-  std::cout << "Enter account holder's name: ";
-  std::cin.ignore();
-  getline(std::cin, account_name);
-
-  std::cout << "\nEnter initial Balance: ";
+  std::cout << "Please enter account name:";
+  std::cin >> account_name;
+  std::cout << "Please enter initial balance:";
   std::cin >> initial_balance;
 
-  const std::string filename = "accounts/" + account_name + ".txt";
+  Account new_account;
+  new_account.account_name = account_name;
+  new_account.account_balance = initial_balance;
 
-  std::ofstream file(filename);
-  file << initial_balance;
+  (*account_db).push_back(new_account);
 
-  if (!file) {
-    std::cerr << "Account creation failed!\n";
-    file.close();
-    return;
-  }
-
-  std::cout << "Account successfully created!" << std::endl;
-
-  file.close();
-
-  return;
+  std::cout << std::endl
+            << "Account successfully created!\n";
 }
 
 void transfer_money() {
-  std::string from_account{""};
-  std::cout << "From (account holder's name): ";
-  std::cin.ignore();
-  std::getline(std::cin, from_account);
-
-  std::string to_account{""};
-  std::cout << "To (account holder's name): ";
-  std::getline(std::cin, to_account);
-
-  double transfer_amount{0};
-  std::cout << "Enter amount to transfer: ";
-  std::cin >> transfer_amount;
-
-  const std::string from_account_name{"./accounts/" + from_account + ".txt"};
-  const std::string to_account_name{"./accounts/" + to_account + ".txt"};
-
-  std::fstream file;
-
-  double from_account_balance{0.0};
-  double to_account_balance{0.0};
-
-  int from_temp_balance{0};
-  int to_temp_balance{0};
-
-  file.open(from_account_name);
-
-  if (!file.is_open()) {
-    std::cerr << "From file could not be opened!" << std::endl;
-    return;
-  }
-
-  while (file >> from_temp_balance) {
-    from_account_balance = from_temp_balance;
-  }
-
-  file.clear();
-  file.close();
-  file.open(to_account_name);
-
-  if (!file.is_open()) {
-    std::cerr << "To file could not be opened!" << std::endl;
-    return;
-  }
-
-  while (file >> to_temp_balance) {
-    to_account_balance = to_temp_balance;
-  }
-
-  file.clear();
-  file.close();
-
-  std::cout << "-----------------------" << std::endl;
-
-  const double new_from_balance = from_account_balance - transfer_amount;
-  const double new_to_balance = to_account_balance + transfer_amount;
-
-  std::ofstream o_file;
-
-  o_file.open(from_account_name);
-  if (!o_file.is_open()) {
-    std::cerr << "From o_file could not be opened!" << std::endl;
-    return;
-  }
-  o_file << new_from_balance;
-  o_file.clear();
-  o_file.close();
-
-  o_file.open(to_account_name);
-  if (!o_file.is_open()) {
-    std::cerr << "To o_file could not be opened!" << std::endl;
-    return;
-  }
-  o_file << new_to_balance;
-  o_file.clear();
-  o_file.close();
+  std::cout << "Transfer Money!" << std::endl;
 }
 
-void view_account_transactions() {
+void view_account_transactions(std::vector<Account> *account_db) {
   std::cout << "View account transactions!" << std::endl;
+  for (size_t i = 0; i < (*account_db).size(); i++) {
+    std::cout << (*account_db).at(i).account_name << std::endl;
+  }
 }
 
 void exit_program() {
-  std::cout << std::endl
-            << "Thank you for using our program. Goodbye..." << std::endl;
+  std::cout << "\nThank you for using our program. Goodbye..." << std::endl;
 }
